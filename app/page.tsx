a475b810205a7ae30126ee633ac725b67e656887
Govardhan K G <govi.reddy420@gmail.com>
2026-02-03 00:31:34 +0530

    modified comps

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="p-4">
+      <div className=" flex flex-col gap-3">
+     <div>
+       <Button variant={"elevated"}>
+          i am button
+      </Button>
+     </div>
+      <div>
+        <Input placeholder="i am text"></Input>
+      </div>
+    <Progress value={40}></Progress>
     <div>
-     <p className="text-rose-500 ">
-      Hello WORLD!!
-     </p>
+      <Textarea placeholder="i am text"></Textarea>
+    </div>
+    </div>
+     
     </div>
   );
 }
