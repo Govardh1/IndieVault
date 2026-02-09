e586c1a3fa099cd9dd11075bf4ae6ba42b4a7200
Govardhan K G <govi.reddy420@gmail.com>
2026-02-09 20:44:56 +0530

    added categories

--- app/(app)/(home)/search-filters/Categories.tsx
+++ app/(app)/(home)/search-filters/Categories.tsx
@@ -8,3 +8,23 @@ export default function Categories(
 	<div>Categoriesprops</div>
   )
 }
+interface Categoriesprops{
+	data:any
+}
+export default function Categories(
+	{data}:Categoriesprops 
+) {
+  return (
+	<div>Categoriesprops</div>
+  )
+}
+interface Categoriesprops{
+	data:any
+}
+export default function Categories(
+	{data}:Categoriesprops 
+) {
+  return (
+	<div>Categoriesprops</div>
+  )
+}
